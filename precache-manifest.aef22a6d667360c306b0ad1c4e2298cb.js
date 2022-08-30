81980e4745c3da4b94d8aedecfb63fd7d335683a
Beastboyshubh <beastboyshubh1912@gmail.com>
2022-08-31 01:01:16 +0530

    Updates

---
+++ precache-manifest.aef22a6d667360c306b0ad1c4e2298cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42aceeccd295f0834579",
+    "url": "/my-app/static/css/main.34d2509c.chunk.css"
+  },
+  {
+    "revision": "42aceeccd295f0834579",
+    "url": "/my-app/static/js/main.42aceecc.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/my-app/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "946d8365c0317c9ba6aa",
+    "url": "/my-app/static/js/2.946d8365.chunk.js"
+  },
+  {
+    "revision": "ad6099f2e84041bd8e59",
+    "url": "/my-app/static/js/runtime~main.ad6099f2.js"
+  },
+  {
+    "revision": "70df1770eb6f3d15b1b401c292e5453c",
+    "url": "/my-app/index.html"
+  }
+];
